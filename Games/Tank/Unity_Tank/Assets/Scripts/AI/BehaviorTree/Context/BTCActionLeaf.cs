7cdb2481674f972999f8e36c4cdc264b4b1b7a3d
jiepengtan <jiepengtan@gmail.com>
2019-07-03 22:10:25 +0800

    [-] Client剥离AI. [+] AssetManager 公有化

--- Games/Tank/Unity_Tank/Assets/Scripts/AI/BehaviorTree/Context/BTCActionLeaf.cs
+++
@@ -1,9 +0,0 @@
-using System.Runtime.InteropServices;
-
-namespace Lockstep.AI {
-    [StructLayout(LayoutKind.Sequential, Pack = NativeHelper.STRUCT_PACK)]
-    public unsafe partial struct BTCActionLeaf {
-        internal int status;
-        internal bool needExit;
-    }
-}
