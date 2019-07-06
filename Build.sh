6a36b0117afee0da303c2fb5643f985f444a41a8
jiepengtan <jiepengtan@gmail.com>
2019-07-06 19:32:04 +0800

    [+] 添加批处理命令

---
+++ Build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#!/bin/bash
+dir="$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)"
+cd $dir
+pwd
+echo " ------------Build LPEngine ...-------------"
+msbuild /property:Configuration=Debug /p:WarningLevel=0 /verbosity:minimal LPEngine.sln
+echo " ------------Build LockstepPlatform ...-------------"
+msbuild /property:Configuration=Debug /p:WarningLevel=0 /verbosity:minimal LockstepPlatform.sln
+echo "Done "
